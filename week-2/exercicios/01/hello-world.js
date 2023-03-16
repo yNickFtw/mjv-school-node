924b9d5849a52fdd630442b7a1d2864e387d71d2
Nicolas Freitas <nkxynickftw@gmail.com>
2023-03-16 17:03:42 -0300

    first commit

---
+++ week-2/exercicios/01/hello-world.js
@@ -0,0 +1,2 @@
+console.log("Meu primeiro arquivo JavaScript no Node!")
+console.log(2 + 2)
---
+++ week-2/primeiro-arquivo-node.js
@@ -0,0 +1,11 @@
+console.log('Primeiro Hello World!')
+
+const numero1 = 15
+const numero2 = 12
+
+const soma = () => {
+    return numero1 + numero2
+}
+
+console.log(`O resultado da soma é: ${soma()}`)
+console.log('A data de hoje é: ', new Date())
